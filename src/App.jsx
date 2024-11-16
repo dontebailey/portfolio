950eeef2999deb37239ffa1e34bbd2541507ea9e
dontebailey <baileydonte27@gmail.com>
2024-11-16 01:05:14 -0500

    started Hero section of page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
+      
       <Navbar />
+      <Hero/>
       <div className="background-gradient"></div>
       <div className="container"></div>
     </div>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import { HERO_CONTENT } from "../constants";
+function Hero() {
+  return (
+    <div className="hero-container">
+      <div className="hero-content">
+        <div className="text-section">
+          <div className="text-section text-content">
+            <h1 className="name">Donte Bailey</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
