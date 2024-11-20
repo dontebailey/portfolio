75eb8cf186578cfefb8027aa4142e438fe20a778
dontebailey <baileydonte27@gmail.com>
2024-11-19 20:54:01 -0500

    got text section and newly added image section lined up correctly in flexbox

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,17 @@ import { HERO_CONTENT } from "../constants";
 function Hero() {
   return (
     <div className="hero-container">
-      <div className="hero-content">
-        <div className="text-section">
-          <h1 className="name">Donte Bailey</h1>
-          <span className="title">Software Developer</span>
-          <p className="intro">{HERO_CONTENT}</p>
-        </div>
+      <div className="text-section">
+        <h1 className="name">Donte Bailey</h1>
+        <span className="title">Software Developer</span>
+        <p className="intro">{HERO_CONTENT}</p>
+      </div>
+
+      <div className="image-section">
+        <img
+          src="src/assets/donteBaileyProfile.jpg"
+          className="profile-image"
+        ></img>
       </div>
     </div>
   );
