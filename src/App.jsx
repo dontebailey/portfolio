5794a1768a21092becd6908283ae2c7268dc2551
dontebailey <baileydonte27@gmail.com>
2024-11-21 22:29:29 -0500

    started about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <About />
       <div className="background-gradient"></div>
     </div>
   );
---
+++ src/components/About.jsx
@@ -0,0 +1,6 @@
+import { ABOUT_TEXT } from "../constants";
+function About() {
+  return <h1 className="about-header">About Me</h1>;
+}
+
+export default About;
