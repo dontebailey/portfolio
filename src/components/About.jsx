2a4e468d4c00c01e6b866f7e9bcd737dea2316fb
dontebailey <baileydonte27@gmail.com>
2024-11-26 22:09:31 -0500

    added picture and text to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,14 @@
 import { ABOUT_TEXT } from "../constants";
 function About() {
-  return <h1 className="about-header">About Me</h1>;
+  return (
+    <div className="about-container">
+      <h1 className="about-header">About Me</h1>
+      <div className="about-image-container">
+        <img src="/aboutPic.jpg" className="about-image"></img>
+        <p className="about">{ABOUT_TEXT}</p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
