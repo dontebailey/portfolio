b69ed5bf93b7054339c5d164fdcc1e81fc8c97d8
dontebailey <baileydonte27@gmail.com>
2024-12-01 01:08:09 -0500

    added more icon

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,5 +1,8 @@
 import { RiReactjsLine } from "react-icons/ri";
-
+import { SiJavascript } from "react-icons/si";
+import { SiPython } from "react-icons/si";
+import { TiHtml5 } from "react-icons/ti";
+import { IoLogoCss3 } from "react-icons/io5";
 function Technologies() {
   return (
     <div className="technologies-container">
@@ -7,6 +10,18 @@ function Technologies() {
       <div className="widget-container">
         <p className="widget-border">
           <RiReactjsLine className="react-icon" />
+        </p>
+        <p className="widget-border">
+          <SiJavascript className="javascript-icon" />
+        </p>
+        <p className="widget-border">
+          <SiPython className="python-icon" />
+        </p>
+        <p className="widget-border">
+          <TiHtml5 className="html-icon" />
+        </p>
+        <p className="widget-border">
+          <IoLogoCss3 className="css-icon" />
         </p>
       </div>
     </div>
