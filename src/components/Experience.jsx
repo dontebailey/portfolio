20be5618eda2772f291fae7c55345ef4755ce0a0
dontebailey <baileydonte27@gmail.com>
2024-12-02 19:57:30 -0500

    finished technologies section and added experience section

---
+++ src/components/Experience.jsx
@@ -0,0 +1,9 @@
+function Experience() {
+  return (
+    <div className="experience-container">
+      <h1 className="experience-header">Experience</h1>
+    </div>
+  );
+}
+
+export default Experience;
