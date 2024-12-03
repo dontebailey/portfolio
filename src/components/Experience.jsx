76cc4f2956b90f1028d0984a1d4659f953350402
dontebailey <baileydonte27@gmail.com>
2024-12-02 22:14:30 -0500

    added experience year and role sections

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,20 @@
+import { EXPERIENCES } from "../constants";
 function Experience() {
   return (
     <div className="experience-container">
       <h1 className="experience-header">Experience</h1>
+      <div className="sections-container">
+        {EXPERIENCES.map((experience, index) => (
+          <div key={index} className="experience">
+            <div className="year-section">
+              <p className="year">{experience.year}</p>
+            </div>
+            <div className="role-section">
+              <h6 className="role">{experience.role}</h6>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
